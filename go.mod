1b7213bf01d617de3cbc0f8724d906aec83d7af9
jc <46619361+juancwu@users.noreply.github.com>
2023-12-05 16:52:18 -0500

    add set of features to search/install pkg

--- go.mod
+++ go.mod
@@ -3,8 +3,15 @@ module github.com/juancwu/gopack
 go 1.21.3
 
 require (
+	github.com/charmbracelet/bubbles v0.16.1
+	github.com/charmbracelet/bubbletea v0.24.2
+	golang.org/x/net v0.19.0
+)
+
+require (
+	github.com/atotto/clipboard v0.1.4 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
-	github.com/charmbracelet/bubbletea v0.24.2 // indirect
+	github.com/charmbracelet/lipgloss v0.7.1 // indirect
 	github.com/containerd/console v1.0.4-0.20230313162750-1ae8d489ac81 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
 	github.com/mattn/go-isatty v0.0.18 // indirect
@@ -16,7 +23,7 @@ require (
 	github.com/muesli/termenv v0.15.1 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	golang.org/x/sync v0.1.0 // indirect
-	golang.org/x/sys v0.6.0 // indirect
-	golang.org/x/term v0.6.0 // indirect
-	golang.org/x/text v0.3.8 // indirect
+	golang.org/x/sys v0.15.0 // indirect
+	golang.org/x/term v0.15.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
--- main.go
+++ main.go
@@ -2,104 +2,236 @@ package main
 
 import (
 	"fmt"
-    "os"
+	"net/http"
+	"net/url"
+	"os"
+	"os/exec"
+	"regexp"
+	"strings"
 
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
+	"golang.org/x/net/html"
 )
 
 type model struct {
-    choices []string
-    cursor int
-    selected map[int]struct{}
+	textInput textinput.Model
+	choices   []string
+	cursor    int
+	startIdx  int
+	err       error
 }
 
+const (
+	pkgSearchUrl = "https://pkg.go.dev/search?m=package&%s"
+)
+
+var (
+    results []string
+	selection string
+	step      int = 0
+)
+
 func initModel() model {
-    return model{
-        choices: []string{"this", "that", "those"},
-        selected: make(map[int]struct{}),
-    }
+	ti := textinput.New()
+	ti.Placeholder = "Package name"
+	ti.Focus()
+	ti.CharLimit = 156
+	ti.Width = 20
+
+	return model{
+		choices:   results,
+		textInput: ti,
+		err:       nil,
+	}
 }
 
 func (m model) Init() tea.Cmd {
-    return nil
+	return nil
+}
+
+func (m model) FirstStepUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
+	switch msg := msg.(type) {
+	case tea.KeyMsg:
+		switch msg.Type {
+		case tea.KeyCtrlC, tea.KeyEsc:
+			return m, tea.Quit
+		case tea.KeyEnter:
+            pkgName := m.textInput.Value()
+            search(pkgName)
+            m.choices = results
+            step++
+		}
+	}
+
+	m.textInput, cmd = m.textInput.Update(msg)
+	return m, cmd
+}
+
+func (m model) SecondStepUpdate(msg tea.Msg) (tea.Model, tea.Cmd) {
+    if len(m.choices) == 0 {
+        return m, tea.Quit
+    }
+
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			selection = getPkgUrl(m.choices[m.cursor])
+			return m, tea.Quit
+		}
+	}
+
+	const pageSize = 5
+	m.startIdx = max(0, min(len(m.choices)-pageSize, m.cursor-(pageSize/2)))
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
-    switch msg := msg.(type) {
-
-    // Is it a key press?
-    case tea.KeyMsg:
-
-        // Cool, what was the actual key pressed?
-        switch msg.String() {
-
-        // These keys should exit the program.
-        case "ctrl+c", "q":
-            return m, tea.Quit
-
-        // The "up" and "k" keys move the cursor up
-        case "up", "k":
-            if m.cursor > 0 {
-                m.cursor--
-            }
-
-        // The "down" and "j" keys move the cursor down
-        case "down", "j":
-            if m.cursor < len(m.choices)-1 {
-                m.cursor++
-            }
-
-        // The "enter" key and the spacebar (a literal space) toggle
-        // the selected state for the item that the cursor is pointing at.
-        case "enter", " ":
-            _, ok := m.selected[m.cursor]
-            if ok {
-                delete(m.selected, m.cursor)
-            } else {
-                m.selected[m.cursor] = struct{}{}
-            }
-        }
-    }
-
-    // Return the updated model to the Bubble Tea runtime for processing.
-    // Note that we're not returning a command.
-    return m, nil
+	if step == 0 {
+		return m.FirstStepUpdate(msg)
+	}
+	return m.SecondStepUpdate(msg)
 }
 
 func (m model) View() string {
-    // The header
-    s := "What should we buy at the market?\n\n"
+	// The header
+	var s string
+	if step == 0 {
+		s = fmt.Sprintf("Enter package name:\n\n%s\n\n%s\n", m.textInput.View(), "(esc to quit)")
+	} else {
+		s = "Choose package to install:\n\n"
 
-    // Iterate over our choices
-    for i, choice := range m.choices {
+		// Iterate over our choices
+		for i, choice := range m.choices[m.startIdx:min(m.startIdx+5, len(m.choices))] {
 
-        // Is the cursor pointing at this choice?
-        cursor := " " // no cursor
-        if m.cursor == i {
-            cursor = ">" // cursor!
-        }
+			// Is the cursor pointing at this choice?
+			cursor := " " // no cursor
+			if m.cursor == m.startIdx+i {
+				cursor = ">" // cursor!
+			}
 
-        // Is this choice selected?
-        checked := " " // not selected
-        if _, ok := m.selected[i]; ok {
-            checked = "x" // selected!
-        }
+			// Render the row
+			s += fmt.Sprintf("%s %s\n", cursor, choice)
+		}
 
-        // Render the row
-        s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		// Number of results
+		s += fmt.Sprintf("\nNumber of results: %d\n", len(m.choices))
+
+		// The footer
+		s += "\nPress q to quit.\n"
+	}
+
+	// Send the UI for rendering
+	return s
+}
+
+func getResults(n *html.Node) {
+	if n.Type == html.ElementNode {
+		for _, a := range n.Attr {
+			if a.Key == "data-gtmv" {
+				// fmt.Printf("Result: %s\n", getText(n))
+				results = append(results, getText(n))
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		getResults(c)
+	}
+}
+
+func getText(n *html.Node) string {
+	if n.Type == html.TextNode {
+		return n.Data
+	}
+
+	var result strings.Builder
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		r := getText(c)
+		if r != "" {
+			result.WriteString(strings.TrimSpace(r))
+		}
+	}
+
+	return result.String()
+}
+
+func getPkgUrl(value string) string {
+	re := regexp.MustCompile(`\((.*?)\)`)
+	match := re.FindStringSubmatch(value)
+	if len(match) > 1 {
+		return match[1]
+	}
+	return match[0]
+}
+
+func search(term string) {
+	params := url.Values{}
+	params.Add("q", term)
+	searchUrl := fmt.Sprintf(pkgSearchUrl, params.Encode())
+	fmt.Printf("URL: %s\n", searchUrl)
+	resp, err := http.Get(searchUrl)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		panic(err)
+	}
+	getResults(doc)
+}
+
+func runGoGet(pkg string) error {
+    fmt.Printf("Running: go get %s\n", pkg)
+    cmd := exec.Command("go", "get", pkg)
+    err := cmd.Run()
+    if err != nil {
+        return err
     }
-
-    // The footer
-    s += "\nPress q to quit.\n"
-
-    // Send the UI for rendering
-    return s
+    return nil
 }
 
 func main() {
-    p := tea.NewProgram(initModel())
-    if _, err := p.Run(); err != nil {
-        fmt.Printf("Alas, there's been an error: %v", err)
-        os.Exit(1)
+	m := initModel()
+	p := tea.NewProgram(m)
+	if _, err := p.Run(); err != nil {
+		fmt.Printf("Alas, there's been an error: %v", err)
+		os.Exit(1)
+	}
+
+    fmt.Printf("Selected: %s\n", selection)
+    err := runGoGet(selection)
+    if err != nil {
+        panic(err)
     }
 }
