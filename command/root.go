2824fac64b314e28b74a59724d676c4148515902
jc <46619361+juancwu@users.noreply.github.com>
2025-01-17 01:30:57 -0500

    remove unused constant

--- command/root.go
+++ command/root.go
@@ -11,8 +11,6 @@ const (
 	cfgType    = "yaml"
 	timeFormat = "20060102150405"
 	timezone   = "UTC"
-
-	dirFlagUsage = "This is the destination the migrations are located. This should be relative to the CWD or an absolute path"
 )
 
 func Execute() error {
